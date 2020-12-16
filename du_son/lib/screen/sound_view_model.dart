4d0ef203afa902f981cfcb9fb8ef135f6377a2ea
Atori Ikeyama <atori.ikeyama@gmail.com>
2020-12-17 01:42:37 +0900

    add
    sound_view_model

---
+++ du_son/lib/screen/sound_view_model.dart
@@ -0,0 +1,11 @@
+import 'package:du_son/models/sound_model.dart';
+import 'package:du_son/util/calculate.dart';
+import 'package:flutter/material.dart';
+
+class SoundManager with ChangeNotifier {
+  SoundModel model = SoundModel();
+  Calculate _calculate = Calculate();
+
+
+
+}
